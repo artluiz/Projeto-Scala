bddee54abfeb14a781721a970de6191141e9a277
Luiz <luizarthursm@gmail.com>
2023-10-09 08:20:05 -0300

    Testes

--- src/Main.scala
+++ src/Main.scala
@@ -3,6 +3,8 @@ import Classes.Calculadora
 object Main {
   def main(args: Array[String]): Unit = {
     val calc = new Calculadora
+
+    //val result = Calculadora.soma(10,20) sem instânciar um objeto
     val result = calc.soma(10,20)
     println(s"O resultado é $result")
   }
