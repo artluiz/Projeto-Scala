f760481b2eb6335cfada4d2157f9a48eabd97cdf
Luiz <luizarthursm@gmail.com>
2023-10-09 08:59:52 -0300

    FInalizando classe calculadora básica

--- src/Classes/Calculadora.scala
+++ src/Classes/Calculadora.scala
@@ -1,10 +1,19 @@
 package Classes
 
+import scala.util.Try
+
 class Calculadora {
 
   def soma(x: Int, y:Int): Int = x+y
+  def + = soma _
 
   def sub(x: Int, y:Int): Int = x-y
+  def - = sub _
 
   def multi(x: Int, y:Int): Int = x*y
+  def * = multi _
+
+  def div(divid: Double, divis: Double): Double = Try(divid / divis).getOrElse(0)
+  def / = div _
+
 }
--- src/Main.scala
+++ src/Main.scala
@@ -5,7 +5,10 @@ object Main {
     val calc = new Calculadora
 
     //val result = Calculadora.soma(10,20) sem instânciar um objeto
-    val result = calc.soma(10,20)
-    println(s"O resultado é $result")
+    println("Soma: " + calc.soma(10, 20))
+    println("Subtração: " + calc.sub(10, 20))
+    println("Multiplicação: " + calc.multi(10, 20))
+    println("Divisão: " + calc.div(10, 20))
   }
+
 }
